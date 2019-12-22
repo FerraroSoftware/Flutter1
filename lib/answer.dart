56ef44895a12966c04058b8d6dcf3b112e5e54d0
FerraroSoftware <ferrarosoftware@gmail.com>
2019-12-22 01:07:45 -0500

    Mapping lists to widgets - yikes

--- lib/answer.dart
+++ lib/answer.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   // Value is function
   final Function selectHandler;
+  final String answerText;
 
   // _answerQuestion() callback passed to constructor as pointer 
   // and stored as selectHandler declared to be static function
-  Answer(this.selectHandler);
+  Answer(this.selectHandler, this.answerText);
   
   @override
   Widget build(BuildContext context) {
@@ -16,7 +17,8 @@ class Answer extends StatelessWidget {
         // Static property - Colors.blue
         // No need to create instance of class Colors
         color: Colors.blue,
-        child: Text('Answer 1'),
+        textColor: Colors.white,
+        child: Text(answerText),
         onPressed: selectHandler,
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,8 @@ void main() => runApp(MyApp());
 // Look for the 3 conenctions for state updating -
 
 class MyApp extends StatefulWidget {
-
-  // !!!Connection 2 
-  // type createState() to auto populate 
+  // !!!Connection 2
+  // type createState() to auto populate
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
@@ -22,7 +21,7 @@ class MyApp extends StatefulWidget {
 }
 
 // Stateful widgets have 2 classes, name is same with 'State' added
-// State is attached to widget and not recreated unlike MyApp() 
+// State is attached to widget and not recreated unlike MyApp()
 // !!!Connection 1 :: extends State<MyApp>
 // MyAppState == public
 // _MyAppState == Can now only be used in this file where this class lives
@@ -30,13 +29,12 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
   void _answerQuestion() {
-    
     // !!!Connection 3 :: using setState()
     // Calls the build() method again
     setState(() {
-      _questionIndex = _questionIndex + 1;  
+      _questionIndex = _questionIndex + 1;
     });
-    
+
     print(_questionIndex);
   }
 
@@ -44,32 +42,48 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s your favoite color?',
-      'What\'s your favorite animal?',
+      // Creating map shorthand, Map alterative
+      {
+        'questionText': 'What\'s your favoite color?',
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': ['Rabbit', 'Dog', 'Elephant', 'Lion']
+      },
+      {
+        'questionText': 'What\'s your favorite food?',
+        'answers': ['Steak', 'Pizza', 'Pasta', 'Chicken']
+      },
     ];
 
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('My first App'),
-          ),
-          body: Column(
-            children: [
-              //!!! Use custom Question widget here
-              // Takes in String
-              Question(
-                questions[_questionIndex],
-              ),
-              // Callback to pass answerQuestion() to Answer Widget
-              // Fowarding pointer to constrctor 
-              Answer(_answerQuestion),
-              Answer(_answerQuestion),
-              Answer(_answerQuestion),
-              ],
-              ),
-          ),
-          );
+        appBar: AppBar(
+          title: Text('My first App'),
+        ),
+        body: Column(
+          children: [
+            //!!! Use custom Question widget here
+            // Takes in String
+            Question(
+              // Access map 0, 1, 2
+              // Then key 'questionText' 
+              questions[_questionIndex]['questionText'],
+            ),
+            
+            // Applies to every element in the map
+            // Need to tell map as List of strings, doesn't beleive me 
+            // ... == spreadoperator
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+
+          ],
+        ),
+      ),
+    );
   }
 }
 
-// Lets create a new Widget called question in a new file 
+// Lets create a new Widget called question in a new file
